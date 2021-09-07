db4f45abfa8b0e90b0fb3820c965bca70ef6f0a2
r.tihovs <r.tihovs@gmail.com>
2021-09-07 21:38:51 +0300

    Add project files.

---
+++ Majasdarbs2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Majasdarbs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
